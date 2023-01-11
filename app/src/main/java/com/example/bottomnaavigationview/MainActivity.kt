87458a8f7ad8a6d7de5a0879ab59c4a5ec244832
Andre <opolloandres@gmail.com>
2023-01-11 20:53:08 +0300

    Final commit

--- app/src/main/java/com/example/bottomnaavigationview/MainActivity.kt
+++ app/src/main/java/com/example/bottomnaavigationview/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.bottomnaavigationview
 
+import android.content.ClipData
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.internal.NavigationMenu
+import com.google.android.material.navigation.NavigationBarMenu
+import com.google.android.material.navigation.NavigationBarMenuView
+import com.google.android.material.navigation.NavigationBarView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentHome = HomeFragment()
+        val fragmentMessage = MessageFragment()
+        val fragmentProfile = ProfileFragment()
+        setCurrentFragment(fragmentHome)
+
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setOnItemSelectedListener{
+            when(it.itemId){
+                R.id.miHome -> setCurrentFragment(fragmentHome)
+                R.id.miMessage->setCurrentFragment(fragmentMessage)
+                R.id.miProfile->setCurrentFragment(fragmentProfile)
+            }
+            true
+        }
+    }
+    private fun setCurrentFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().apply{
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
     }
 }
